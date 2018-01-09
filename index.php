0ed1a8b98eaaf51fdff4096ced13a6ef0fb5f1b5
Fred Scott <fscott@southeast.edu>
2018-01-09 12:32:03 -0600

    added form

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
-//1525 Program 1
-    $var = 'changing the original repo?';
+//1525 first GIT repo
+
 ?>
 <html>
     <head>
@@ -9,6 +9,20 @@
         <title>Change this to your name, not "your name" but your name right?</title>
     </head>
     <body>
+
+        <form action="target.php" method="get">
+
+                <label>Things</label>
+                <input type="text" name="things"><br>
+
+                <label>Stuff</label>
+                <input type="number" name="anum"><br>
+
+
+                <label>&nbsp;</label>
+                <input type="submit" value="Send"><br>
+
+        </form>
 
     </body>
 </html>
