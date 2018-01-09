40a0f29671945bc4e52e17191155e7c8053478b1
lw244130 <lw244130@LR00905.southeast.edu>
2018-01-09 14:12:41 -0600

    added target page to get variables

---
+++ target.php
@@ -0,0 +1,12 @@
+<?php
+$things = filter_input(INPUT_GET, 'things');
+$stuff = filter_input(INPUT_GET, 'anum', FILTER_VALIDATE_INT);
+
+?>
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+<html>
